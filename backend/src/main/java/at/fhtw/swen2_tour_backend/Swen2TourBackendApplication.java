63380f49629a3217d014a708fd390bf867989e12
mzn-b <medomedso300@gmail.com>
2022-04-28 13:23:48 +0200

    initial SpringBoot commit

---
+++ backend/src/main/java/at/fhtw/swen2_tour_backend/Swen2TourBackendApplication.java
@@ -0,0 +1,13 @@
+package at.fhtw.swen2_tour_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Swen2TourBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Swen2TourBackendApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/at/fhtw/swen2_tour_backend/Swen2TourBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package at.fhtw.swen2_tour_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Swen2TourBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
